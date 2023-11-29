5db47fc38fc31d13a955034988651aabf3af92c8
ouchi-taiga <ouchi421taiga@gmail.com>
2023-11-29 22:49:43 +0900

    モーダルの微調整、firebaseの導入

--- src/component/Button/GoogleSignInButton/GoogleSignInButton.jsx
+++ src/component/Button/GoogleSignInButton/GoogleSignInButton.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import { FcGoogle } from "react-icons/fc";
 import styles from './GoogleSignInButton.module.css'
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../../../firebase';
 
-const GoogleSignInButton = (props) => {
-    return (
-      <>
-        <button
-          className={styles.googleSignInButton}
-          onClick={props.onClick}
-        >
-          <div className={styles.titleArea}>
-            <div className={styles.title}>Googleでサインイン</div>
-            <FcGoogle
-              className={styles.googleIcon}
-              size={'2.3rem'}
-            />
-          </div>
-        </button>
-      </>
-    )
+const GoogleSignInButton = () => {
+  const SignInWithGoogle = () => {
+    //Googleでサインイン
+    signInWithPopup(auth, provider)
   }
+
+  return (
+    <>
+      <button
+        className={styles.googleSignInButton}
+        onClick={SignInWithGoogle}
+      >
+        <div className={styles.titleArea}>
+          <div className={styles.title}>Googleでサインイン</div>
+          <FcGoogle
+            className={styles.googleIcon}
+            size={'2.3rem'}
+          />
+        </div>
+      </button>
+    </>
+  )
+}
 
 export default GoogleSignInButton
--- src/component/SignInModal/SignInModal.jsx
+++ src/component/SignInModal/SignInModal.jsx
@@ -1,14 +1,65 @@
 import React from 'react'
 import styles from './SignInModal.module.css'
-import { SignInTabs } from '../index.js'
+import { SubmitButton, MailInputForm, PasswordInputForm, GoogleSignInButton } from '../index.js'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 
 
 const SignInModal = (props) => {
   const className  = styles[props.className];
 
+  const handleSelect = (index, last) => {
+    console.log('Selected tab: ' + index + ', Last tab: ' + last);
+  }
+
   return (
     <div className={className}>
-      <SignInTabs />
+      <Tabs
+        className={styles.tabs}
+        onSelect={handleSelect}
+        defaultIndex={0}
+        selectedTabClassName={styles.activeTab}
+      >
+        <TabList  className={styles.tabList}>
+          <div className={styles.title}></div>
+          <Tab className={styles.tabListButton1}>Sign in</Tab>
+          <Tab className={styles.tabListButton2}>Sign up</Tab>
+        </TabList>
+
+        <TabPanel className={styles.tabPanel}>
+          <div className={styles.signInTabContent}>
+            <div className={styles.mailInputForm}>
+              <MailInputForm />
+            </div>
+            <div className={styles.passwordInputForm}>
+              <PasswordInputForm />
+            </div>
+            <div className={styles.submitButton}>
+              <SubmitButton />
+            </div>
+            <hr className={styles.horizontalLine}/>
+            <div className={styles.googleSignInButton}>
+              <GoogleSignInButton />
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel className={styles.tabPanel}>
+          <div className={styles.signUpTabContent}>
+            <div className={styles.mailInputForm}>
+              <MailInputForm />
+            </div>
+            <div className={styles.passwordInputForm}>
+              <PasswordInputForm />
+            </div>
+            <div className={styles.submitButton}>
+              <SubmitButton />
+            </div>
+            <hr className={styles.horizontalLine}/>
+            <div className={styles.googleSignInButton}>
+              <GoogleSignInButton />
+            </div>
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
   )
 }
